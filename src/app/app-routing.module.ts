92393909a4cdefdc83db652697324d8c5bf8034d
Ayoub EL Bouasri <ayoub.elbouuasri@gmail.com>
2023-01-24 16:20:06 +0100

    comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,39 +4,30 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddproductComponent } from './addproduct/addproduct.component';
 
 import { AuthentificationComponent } from './authentification/authentification.component';
-import { CostumerComponent } from './costumer/costumer.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
 import { ProductComponent } from './product/product.component';
 
-
 const routes: Routes = [
-
   {
-    path: "authentification",
-    component: AuthentificationComponent 
-  } ,
-  {
-    path: "product",
-    component: ProductComponent
-  } ,
-  {
-    path: "costumer",
-    component: CostumerComponent
+    path: 'authentification',
+    component: AuthentificationComponent,
   },
   {
-    path: "edit-product",
-    component: EditProductComponent
+    path: 'product',
+    component: ProductComponent,
   },
   {
-    path: "addProduct",
-    component: AddproductComponent
+    path: 'edit-product',
+    component: EditProductComponent,
   },
-  
-  
+  {
+    path: 'addProduct',
+    component: AddproductComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {  HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthentificationComponent } from './authentification/authentification.component';
 import { ProductComponent } from './product/product.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { CostumerComponent } from './costumer/costumer.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
 import { AddproductComponent } from './addproduct/addproduct.component';
-
-
 
 @NgModule({
   declarations: [
     ProductComponent,
     NavbarComponent,
-    CostumerComponent,
     EditProductComponent,
     AddproductComponent,
     AppComponent,
     AuthentificationComponent,
- 
-   
   ],
   imports: [
     ReactiveFormsModule,
     FormsModule,
-   HttpClientModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
